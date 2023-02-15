4999d543683f6bfcff79a09c4f35f68877d61ddf
kollawat rupanya <micky2smart1@gmail.com>
2023-02-16 00:43:43 +0700

    to-do list can create now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
 
-
-
-
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
 
   return (
     <div className="container">
@@ -13,14 +22,19 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input 
+        onChange={handleChange} 
+        type="text" 
+        value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map(todoItem => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
