ced6c85098ef5f9dee6d0a092e88cd9c5cd428f4
kollawat rupanya <micky2smart1@gmail.com>
2023-02-17 01:26:12 +0700

    final version

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function ToDoItem(props){
-  const [isDone,setIsDone] = useState(false)
+// function ToDoItem(props){
+//   const [isDone,setIsDone] = useState(false)
 
-  function handleOnclick(){
-    setIsDone(prevValue =>{
-      return !prevValue
-    })
-  }
+//   function handleOnclick(){
+//     setIsDone(prevValue =>{
+//       return !prevValue
+//     })
+//   }
 
-  return <div onClick={handleOnclick}>
-    <li style = {{textDecoration : isDone? "line-through":"none"}}>
-    {props.text}
-  </li>
-  </div>
+//   return <div onClick={handleOnclick}>
+//     <li style = {{textDecoration : isDone? "line-through":"none"}}>
+//     {props.text}
+//   </li>
+//   </div>
+// }
+
+// export default ToDoItem
+
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
 }
 
-export default ToDoItem
+export default ToDoItem;
